921b34f5f127debf24233fbc38f868654e3dd7ba
BuzzFid <abdalmalek2005@gmail.com>
2025-12-03 14:08:05 +0300

    Add project files.

---
+++ Models/Book.cs
@@ -0,0 +1,22 @@
+﻿namespace MyFavBookApp.Models
+{
+    public class Book
+    {
+        public int id { get; set; }
+
+        public string title { get; set; }
+
+        public string author { get; set; }
+
+        public int publicationYear { get; set; }
+
+        public string description { get; set; }
+
+        public string url { get; set; }
+
+        public Book()
+        {
+            
+        }
+    }
+}
